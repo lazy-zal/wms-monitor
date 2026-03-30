9dca6cbb0ce3e22674aa7bd532e2e9d80dd783a4
lazy-zal <kayatopan@gmail.com>
2026-03-30 09:43:05 +0700

    refactor

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
-import { firebaseConfig } from "/data.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8V6_Rj5xsM8fPDL04FMORkrdygL2yV2o",
+    authDomain: "wms-helper-5c6ca.firebaseapp.com",
+    projectId: "wms-helper-5c6ca",
+    storageBucket: "wms-helper-5c6ca.firebasestorage.app",
+    messagingSenderId: "425367035686",
+    appId: "1:425367035686:web:7d89def2b71ca5de57abe7"
+};
 
 // 1. Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
